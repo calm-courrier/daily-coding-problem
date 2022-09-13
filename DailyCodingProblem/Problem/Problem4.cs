2514c788093e14818518dd8747b04451986bbfe2
Calm Courrier <calmcourrier@gmail.com>
2022-09-13 17:04:53 +0200

    Problem 4

---
+++ DailyCodingProblem/Problem/Problem4.cs
@@ -0,0 +1,27 @@
+﻿namespace DailyCodingProblem.Problem;
+
+public static class Problem4 {
+    public static int Solve(int[] nums) {
+        for (var i = 0; i < nums.Length; i++) {
+            // Number out of range => skip
+            if (nums[i] <= 0 || nums[i] > nums.Length) {
+                continue;
+            }
+
+            // Correct position => continue
+            if (nums[i] == i + 1) continue;
+
+            // Wrong position => swap with right position and process this index again
+            (nums[nums[i] - 1], nums[i]) = (nums[i], nums[nums[i] - 1]);
+            i--;
+        }
+
+        for (var i = 0; i < nums.Length; i++) {
+            if (nums[i] != i +1) {
+                return i+1;
+            }
+        }
+
+        return nums.Length + 1;
+    }
+}
---
+++ DailyCodingProblemTest/Problem4Test.cs
@@ -0,0 +1,29 @@
+﻿using DailyCodingProblem.Problem;
+
+namespace DailyCodingProblemTest;
+
+public class Problem4Test {
+    [Fact]
+    public void Test1() {
+        var solution = Problem4.Solve(new[] { 3, 4, -1, 1 });
+        Assert.Equal(2, solution);
+    }
+
+    [Fact]
+    public void Test2() {
+        var solution = Problem4.Solve(new[] { 1, 2, 0 });
+        Assert.Equal(3, solution);
+    }
+
+    [Fact]
+    public void Test3() {
+        var solution = Problem4.Solve(new[] { 3, 2, 1, 0 });
+        Assert.Equal(4, solution);
+    }
+
+    [Fact]
+    public void Test4() {
+        var solution = Problem4.Solve(new[] { 3, 2, 4, -1, 1 });
+        Assert.Equal(5, solution);
+    }
+}
